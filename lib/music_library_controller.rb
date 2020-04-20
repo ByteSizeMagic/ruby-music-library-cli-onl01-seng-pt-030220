eec62b73e1ec72c1e9ea961a7172fc1e9f8ab2c8
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-20 10:01:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,7 +6,7 @@ class MusicLibraryController
   end
 
   def call
-<<<<<<< HEAD
+
     user_input = ""
 
     while user_input != 'exit'
@@ -40,19 +40,6 @@ class MusicLibraryController
         "Type in a valid request please"
       end
     end
-=======
-    user_input = gets.strip
-
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
->>>>>>> e78bda7d1962578cc12d187976c901bf6d10db3f
   end
 
 end
